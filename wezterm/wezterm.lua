bff99a80f51cb9db40d7f0af0a6b3c6d7eec49c9
Kujoxer <kujoxer@gmail.com>
2024-07-30 10:37:53 +0000

    30.07.2024

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,59 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local act = wezterm.action
+
+-- This is where you actually apply your config choices
+
+-- lanuch zsh by default
+config.default_prog = { "/usr/bin/bash", "-l" }
+-- config.default_prog = { 'powershell', '-NoLogo' }
+
+config.detect_password_input = true
+config.window_background_opacity = 0.85
+
+config.font = wezterm.font_with_fallback({
+	--"SpaceMono Nerd Font Mono",
+	"JetBrainsMono Nerd Font Mono",
+	--"Hack Nerd Font Mono",
+	--"Cascadia Mono",
+	-- "FiraCode Nerd Font",
+})
+config.font_size = 18.0
+
+config.enable_tab_bar = true
+
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 10,
+	bottom = 10,
+}
+
+config.window_close_confirmation = "NeverPrompt"
+
+-- cursor
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 600
+config.cursor_blink_ease_out = "Linear"
+
+-- keymappings
+config.keys = {
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Next"),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
